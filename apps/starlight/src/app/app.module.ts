998fa7319dd2b913cde831d99bc8eb1205c45513
dylanjhall <dylanjhall@gmail.com>
2021-05-04 15:42:28 -0400

    first lib

--- apps/starlight/src/app/app.module.ts
+++ apps/starlight/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {CalendarModule} from '@starlight-components/calendar';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, CalendarModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  projects: ['<rootDir>/apps/starlight'],
+  projects: ['<rootDir>/apps/starlight', '<rootDir>/libs/calendar'],
 };
