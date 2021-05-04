076f6d94a677e867b10075e7b60bba2262e98187
dylanjhall <dylanjhall@gmail.com>
2021-05-04 15:04:05 -0400

    initial commit

---
+++ apps/starlight/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'starlight-components-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'starlight';
+}
